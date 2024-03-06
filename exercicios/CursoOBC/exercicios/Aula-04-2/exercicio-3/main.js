9dd5743d3b39e2ded47a93e533f2ad42c416329d
Arthur Resende <arthur.resendecoura@hotmail.com>
2024-03-05 21:58:53 -0300

    Finished exercise 04.

--- exercicios/CursoOBC/exercicios/Aula-04-2/exercicio-3/main.js
+++ exercicios/CursoOBC/exercicios/Aula-04-2/exercicio-3/main.js
@@ -9,14 +9,18 @@ alert(kebabCaseTitle);
 
 const hoje = new Date();
 
-const diaAniversario = parseInt(prompt(`Digite o dia do seu aniversário`));
-const mesAniversario = parseInt(prompt(`Digite o mês do seu aniversário`));
+const diaAniversario = prompt("Digite o dia do seu aniversário:");
+const mesAniversario = prompt("Digite o mês do seu aniversário (1-12):");
 
 const dataAniversario = new Date(hoje.getFullYear(), mesAniversario - 1, diaAniversario);
-const diferencaDias = Math.floor((dataAniversario - hoje) / (1000 * 60 * 60 * 24));
+
+if (dataAniversario < hoje) {
+dataAniversario.setFullYear(dataAniversario.getFullYear() + 1);
+}
+
+const diferencaDias = Math.floor((hoje - dataAniversario) / (1000 * 60 * 60 * 24));
+
 const diferencaMeses = Math.floor(diferencaDias / 30.44);
 
-alert(`Diferença em dias: ${diferencaDias}`);
-alert(`Diferença em meses: ${diferencaMeses}`);
-
-console.log(`${dataAniversario}`);
+alert(`Diferença em dias: ${Math.abs(diferencaDias)}`);
+alert(`Diferença em meses: ${Math.abs(diferencaMeses)}`);
