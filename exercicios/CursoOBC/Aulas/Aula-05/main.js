998107ae90e54af2a056f699c927900e87d8fc2c
Arthur Resende <arthur.resendecoura@hotmail.com>
2024-03-09 19:18:52 -0300

    Watched "Aula-05"

--- exercicios/CursoOBC/Aulas/Aula-05/main.js
+++ exercicios/CursoOBC/Aulas/Aula-05/main.js
@@ -34,3 +34,41 @@ alert(duasPrimeiras)
 
 frutas.splice(2, 4, " limão") //Ele modifica o array original!
 alert(frutas)
+
+const numList = [3,7,8,4,2,9,7,]
+const dobroDosNumeros = numList.map(function (elementoAtual, indiceAtual, arrayOriginal){
+  console.log(`índice: ${indiceAtual} | Valor: ${elementoAtual}`);
+  return { dobro: elementoAtual * 2, metade: elementoAtual / 2, valorOriginal: elementoAtual}
+})
+
+console.log(dobroDosNumeros);
+
+const numList2 = [3,7,4,9,10,3]
+
+const pares = numList2.filter(function (elementoAtual){
+  return elementoAtual % 2 === 0  //verifica se o número é par
+})
+
+console.log(numList2, pares);
+
+const pessoas = [
+  { id: 100, nome: "Isaac", idade: 28  },
+  { id: 101, nome: "Fernando", idade: 27  },
+  { id: 102, nome: "Leonardo", idade: 19  },
+  { id: 103, nome: "Gabriel", idade: 25  },
+]
+
+let idadeMaxima = parseFloat(prompt(`Escolha uma idade máxima:`))
+
+const pessoasDentroDoLimite = pessoas.filter(function(pessoa){
+  return pessoa.idade<=idadeMaxima
+})
+console.log(pessoasDentroDoLimite)
+
+let nomePesquisado = prompt(`Pesquisar por nome:`)
+
+const pessoaEncontrada = pessoas.find(function (pessoa){
+  return pessoa.nome === nomePesquisado 
+})
+
+console.log(pessoaEncontrada)
