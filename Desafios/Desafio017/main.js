637431d381ddfaed6e477e0e6c682875a0de2941
Arthur Resende <arthur.resendecoura@hotmail.com>
2024-03-15 18:56:46 -0300

    Finalizado exercício lista de tarefas em JS (Desafio017) - referente à aula 06 do curso gratuito de JavaScript da Onebitcode.

---
+++ Desafios/Desafio017/main.js
@@ -0,0 +1,41 @@
+function toKebabCase(str){
+  return str.split(" ").join("-").toLowerCase()
+}
+
+const form = document.getElementById("addTask")
+const taskList = document.getElementById("taskList")
+
+const task = [];
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  const inputTask = document.getElementById("task");
+  const userTaskText = inputTask.value;
+  const deadline = document.getElementById("deadline");
+  const userTaskDeadline = deadline.value;
+
+  const newForm = document.createElement("form");
+  const checkboxLabel = document.createElement("label");
+  checkboxLabel.textContent = ` ${userTaskText} até ${userTaskDeadline.toLowerCase()}`
+  const checkbox = document.createElement("input");
+  checkbox.type = 'checkbox';
+  
+  taskList.appendChild(newForm);
+  newForm.appendChild(checkbox);
+  newForm.appendChild(checkboxLabel);
+  checkboxLabel.htmlFor = userTaskText;
+  checkbox.name = toKebabCase(userTaskText);
+
+  event.target.reset();
+
+  checkbox.addEventListener("change", function() {
+    if (checkbox.checked) {
+      checkboxLabel.innerHTML = `<del>${checkboxLabel.textContent}</del>`;
+    } else {
+      checkboxLabel.innerHTML =  `${checkboxLabel.textContent}`
+    }
+  });
+})
+
+
+
