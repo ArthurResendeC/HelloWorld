c3b493c8c5a5cdf48ff9534393db4a79624fb21b
Arthur Resende <arthur.resendecoura@hotmail.com>
2024-03-23 21:14:15 -0300

    I'm done with this Aula-07 Will review the content later, moving on to next subject...

--- exercicios/CursoOBC/exercicios/Aula-07/main.js
+++ exercicios/CursoOBC/exercicios/Aula-07/main.js
@@ -1,35 +1,47 @@
 import { Random } from "random-js";
 
 const botao = document.getElementById("botaoAdicionar");
-const botaoGerar = document.getElementById("botaoGerar")
+const botaoGerar = document.getElementById("botaoGerar");
 const mensagem = document.getElementById("mensagemCarrinho");
 const paragrafoNumero = document.getElementById("randomNumber");
 const random = new Random();
 
-botao.addEventListener("click", function(){
-  mensagem.style.display = "block"
-  setTimeout(function (){
-    mensagem.style.display = "none"
-  }, 5 * 1000)
-})
+botao.addEventListener("click", function () {
+  mensagem.style.display = "block";
+  setTimeout(function () {
+    mensagem.style.display = "none";
+  }, 5 * 1000);
+});
 
-function gerarNumeros(){
-  const value = random.integer(1,999999);
+let numerosAleatorios = [];
+let numeroGerados = 1;
+
+function gerarNumeros(quantidade = 10) {
+  const value = random.integer(1, 999999);
   let valueString = value.toString();
   while (valueString.length < 6) {
-  valueString = '0' + valueString;
+    valueString = "0" + valueString;
   }
   const valueWithZeros = valueString;
-  paragrafoNumero.innerText = `Número aleatório: ${valueWithZeros}`
+  numerosAleatorios.push(valueWithZeros);
+  paragrafoNumero.innerText = `Número ${numeroGerados} de ${quantidade}: ${valueWithZeros}`;
+  console.log(numerosAleatorios);
+
+  if (numeroGerados >= quantidade) {
+    clearInterval(intervaloID);
+    console.log(`Quantidade de números gerados: ${numeroGerados}`);
+    return;
+  }
+  numeroGerados++;
 }
 
 let intervaloID;
 
-botaoGerar.addEventListener("click", function(){
-  gerarNumeros()
-  clearInterval(intervaloID)
-  intervaloID = setInterval(gerarNumeros, 10 * 1000);
-})
+botaoGerar.addEventListener("click", function () {
+  gerarNumeros();
+  clearInterval(intervaloID);
+  intervaloID = setInterval(gerarNumeros, 3 * 1000);
+});
 
 async function temporizador(tempoEmMilisegundos) {
   return new Promise((resolve, reject) => {
@@ -39,13 +51,13 @@ async function temporizador(tempoEmMilisegundos) {
   });
 }
 
-(async function() {
+(async function () {
   await temporizador(3000);
   console.log("Alerta 1!");
 
-  await temporizador(5000);   
+  await temporizador(5000);
   console.log("Alerta 2!");
 
-  await temporizador(7000); 
+  await temporizador(7000);
   console.log("Alerta 3!");
 })();
