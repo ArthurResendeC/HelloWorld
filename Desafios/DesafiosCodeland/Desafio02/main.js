5b9beb97c69383bf2a6af5fe0fa37a707cbc9d4d
Arthur Resende <arthur.resendecoura@hotmail.com>
2024-03-31 18:46:09 -0300

    Finalizado desafio 2 - Codeland

--- Desafios/DesafiosCodeland/Desafio02/main.js
+++ Desafios/DesafiosCodeland/Desafio02/main.js
@@ -2,6 +2,8 @@ import  Typed  from "typed.js";
 
 var typed = new Typed('#typed',{
     stringsElement:'#typed-strings',
-    typeSpeed: 50
+    typeSpeed: 50,
+    showCursor: false,
+    backDelay: 10000,
+    loop: true
 });
-
