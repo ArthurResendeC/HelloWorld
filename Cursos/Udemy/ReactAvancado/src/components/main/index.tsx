3c5394f6ba6207b3a209fa429f7fcddccb9fb25c
Arthur Resende <arthur.resendec@gmail.com>
2025-01-27 20:21:15 -0300

    chore: set up basic things for advancedReact course application

---
+++ Cursos/Udemy/ReactAvancado/src/components/main/index.tsx
@@ -0,0 +1,6 @@
+const Main = () => (
+  <main className="bg-slate-700 text-white w-full h-full flex flex-col text-center justify-center items-center p-3">
+    <h1>React Avançado</h1>
+  </main>
+)
+export default Main
