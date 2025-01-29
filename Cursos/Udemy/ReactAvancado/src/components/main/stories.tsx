40a3fa105423420f986455e92341617dded2a9c7
Arthur Resende <arthur.resendec@gmail.com>
2025-01-28 23:05:46 -0300

    feat: add Storybook configuration and stories for Main component

---
+++ Cursos/Udemy/ReactAvancado/src/components/main/stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Main from '.';
+import '@/app/globals.css';
+
+export default {
+  title: 'Main',
+  component: Main,
+  
+} as Meta;
+
+export const Default : StoryObj = {};
