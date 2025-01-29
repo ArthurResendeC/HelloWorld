43029a922f75f0aa5990e2a2a514b703630023a9
Arthur Resende <arthur.resendec@gmail.com>
2025-01-28 21:56:22 -0300

    Test

--- Cursos/Udemy/ReactAvancado/src/components/main/index.tsx
+++ Cursos/Udemy/ReactAvancado/src/components/main/index.tsx
@@ -1,6 +1,7 @@
-const Main = () => (
-  <main className="bg-slate-700 text-white w-full h-full flex flex-col text-center justify-center items-center p-3">
-    <h1>React Avançado</h1>
-  </main>
-)
-export default Main
+export default function Main() {
+  return (
+    <main className="bg-slate-700 text-white w-full h-full flex flex-col text-center justify-center items-center p-3">
+      <h1>React Avançado</h1>
+    </main>
+  )
+}
