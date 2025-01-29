74d4135178b02e6005565b36aeb47609cfcf4ee6
Arthur Resende <arthur.resendec@gmail.com>
2025-01-28 21:53:52 -0300

    feat: add husky to advancedReact course application

--- Cursos/Udemy/ReactAvancado/src/components/main/test.tsx
+++ Cursos/Udemy/ReactAvancado/src/components/main/test.tsx
@@ -11,4 +11,9 @@ describe('<Main />', () => {
     // gerar snapshot
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+    expect(container.firstChild).toHaveStyle({ 'background-color': 'rgb(51 65 85 / var(--tw-bg-opacity, 1))' })
+  })
 })
